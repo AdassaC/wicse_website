582204352bf1083e0aa373996a8159367fad53dc
AdassaC <80273492+AdassaC@users.noreply.github.com>
2021-11-03 15:18:25 -0400

    create view file for colleges in database

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -6,4 +6,10 @@ class CollegesController < ApplicationController
         @colleges = College.all
       end
   end
+
+  def show
+    @college = College.find(params[:id])
+    #@courses = @college.courses
+  end
+
 end
